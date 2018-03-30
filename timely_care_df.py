50795a53385df3301a1d77ba43ff7d8b043da103
Janaki <jdkora@gmail.com>
2018-03-30 10:16:31 -0700

    Cleaning of visuals branch

--- timely_care_df.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import csv
-csvpath = "Timely and Effective Care - Hospital.csv"
-timely_care_original_df=pd.read_csv(csvpath,encoding="latin-1")
-del timely_care_original_df["Address"]
-del timely_care_original_df["Phone Number"]
-timely_care_df = timely_care_original_df.loc[timely_care_original_df["State"]=="CA",:]
