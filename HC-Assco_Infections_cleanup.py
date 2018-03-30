08ecc1431ca8e7be1e908acd7cc4f4b43524aedf
Janaki <jdkora@gmail.com>
2018-03-30 10:12:13 -0700

    Cleaning visuals branch

--- HC-Assco_Infections_cleanup.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-HealthCare_Infections = pd.read_csv("Healthcare Associated Infections - Hospital.csv")
-HealthCare_Infections.drop(['Address', 'City', 'ZIP Code', 'Phone Number'], axis = 1)
-HealthCare_Infections_CA = HealthCare_Infections[HealthCare_Infections["State"] == "CA"]
-
-
